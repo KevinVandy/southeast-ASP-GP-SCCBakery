f42751b0c0390f360130d14a3140e56deb3aec09
sw262248 <sw262248@LN01014>
2018-08-22 19:15:45 -0500

    Finished Order class, added forgien keys to C# classes

--- SCCBakery/SCCBakery/Models/Order.cs
+++ SCCBakery/SCCBakery/Models/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,14 @@ namespace SCCBakery.Models
 {
     public class Order
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderID { get; set; }
+
+        public AccountModel TheAccount {get; set;}
+
+        [Required]
+        public int CustomerID { get; set; }
+
+        public int OrderTotal { get; set; }
     }
 }
