827a8afa3488e9b81c40d81593f01db50f7d8afa
sw262248 <sw262248@LN01014>
2018-08-29 18:42:48 -0500

    Added Product View

---
+++ SCCBakery/SCCBakery/Migrations/201808292315540_updatedproducts.cs
@@ -0,0 +1,18 @@
+namespace SCCBakery.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedproducts : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "ImagePath", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "ImagePath");
+        }
+    }
+}
