eecfebc3f3a2e05e6ba6b702dce7deb66b759bca
sw262248 <sw262248@LN01014>
2018-09-05 18:57:44 -0500

    updated order

---
+++ SCCBakery/SCCBakery/Migrations/201809052351478_updateOrder.cs
@@ -0,0 +1,16 @@
+namespace SCCBakery.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateOrder : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
