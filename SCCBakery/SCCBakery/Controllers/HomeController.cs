20c3e4ea2598d124e73563342a32d8ebd1e380d1
kv665238 <kv665238@LN01009>
2018-09-12 19:52:39 -0500

    home page

--- SCCBakery/SCCBakery/Controllers/HomeController.cs
+++ SCCBakery/SCCBakery/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SCCBakery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,12 @@ namespace SCCBakery.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.AProduct.ToList());
+            //return View();
         }
 
         public ActionResult About()
